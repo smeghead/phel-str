20986ecc103f139a6f8ae5a3c31006a4331047e1
smeghead <smeghead7@gmail.com>
2024-04-12 22:17:56 +0900

    feat: implement replace.

--- data/.cache/gacela-custom-services.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  '\\Phel\\Console\\Infrastructure\\ConsoleBootstrap::getFactory' => '\\Phel\\Console\\ConsoleFactory',
-  '\\Phel\\Formatter\\Infrastructure\\Command\\FormatCommand::getConfig' => '\\Phel\\Formatter\\FormatterConfig',
-  '\\Phel\\Run\\Infrastructure\\Command\\ReplCommand::getFactory' => '\\Phel\\Run\\RunFactory',
-  '\\Phel\\Run\\Infrastructure\\Command\\TestCommand::getFacade' => '\\Phel\\Run\\RunFacade',
-  '\\Phel\\Command\\Domain\\Finder\\ComposerVendorDirectoriesFinder::getFacade' => '\\Phel\\Command\\CommandFacade',
-);
